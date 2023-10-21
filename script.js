b1c75159597f510de2e555a3552f39731b2800a9
aydincansu1 <cansu.aydin@outlook.com.tr>
2023-10-21 20:54:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll (".movie-list");
+
+arrows.forEach((arrow, i) => {
+const widthRatio= Math.floor(window.innerWidth/300)
+
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll("img").length;
+  arrow.addEventListener("click", function () {
+    clickCounter++;
+
+    if (imageItem - (6 + clickCounter)+ (6-widthRatio) >= 0) {
+      
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter=0;
+    }
+  });
+});
+
+
+const ball = document.querySelector(".toggle-ball");
+const items =document.querySelectorAll(".container,.navbar,.sidebar,.movie-list-title,.sidebar i,.toggle-ball,.toggle,.movie-list-filter select")
+
+ball.addEventListener("click", function(){
+    items.forEach((item) =>item.classList.toggle("active"));
+})
